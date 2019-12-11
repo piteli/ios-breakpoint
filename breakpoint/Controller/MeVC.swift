c7b76604d18f56010e081c6fcf2d3a8756dc9e83
piteli <rushwender@gmail.com>
2019-12-11 11:22:10 +0800

    updated

---
+++ breakpoint/Controller/MeVC.swift
@@ -0,0 +1,23 @@
+ //
+//  MeVC.swift
+//  breakpoint
+//
+//  Created by Fitri San on 11/12/2019.
+//  Copyright © 2019 Fitri San. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func signOutBtnWasPressed(_ sender: Any) {
+    }
+    
+}
