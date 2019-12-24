e349bbb040bdd90dd37a76e6900be4a847636afb
piteli <rushwender@gmail.com>
2019-12-24 21:37:18 +0800

    updated

--- breakpoint/Controller/CreateGroupsVC.swift
+++ breakpoint/Controller/CreateGroupsVC.swift
@@ -19,6 +19,8 @@ class CreateGroupsVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
     @IBAction func doneBtnWasPressed(_ sender: Any) {
@@ -26,4 +28,24 @@ class CreateGroupsVC: UIViewController {
     
     @IBAction func closeBtnWasPressed(_ sender: Any) {
     }
+}
+
+extension CreateGroupsVC: UITableViewDelegate, UITableViewDataSource{
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "userCell") as? UserCell else { return UITableViewCell() }
+        let profileImage = UIImage(named : "defaultProfileImage")
+        
+        cell.configureCell(profileImage: profileImage!, email: "piteli", isSelected: true)
+        return cell
+    }
+    
 }
